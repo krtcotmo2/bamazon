6c893e92df919aadb9eb1bfe6f010ff171092ef1
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-10 20:59:36 -0500

    Added the new product feature

--- index.js
+++ index.js
@@ -267,7 +267,6 @@ let orderInv = function () {
           });
      });
 }
-
 function addInventory(id, qty){
      let curQty = 0;
      con = sql.createConnection(conOpts);
@@ -304,7 +303,118 @@ function addInventory(id, qty){
 
 
 let newProd = function () {
+     let depts= [];
+     con = sql.createConnection(conOpts);
+     con.connect (function(err){
+          if(err) throw err;
+     });
+     let getDeptsRet = async function(){          
+          let result = await getDepts();
+          result.forEach( o =>{
+               let dept = {
+                    name: o.deptName,
+                    value:o.deptID
+               }
+               depts.push(dept);
+          });
+          con.end();
+          inq.prompt(qs)
+          .then(function(response){
+               confirmAdd(response);
+          })
+     }
+     function getDepts(){
+          return new Promise(resolve => {
+               con.query(`SELECT deptID, deptName FROM department`, function (err, results) {
+                    if (err) throw err;
+                    resolve(results);
+               });
+          })
+     }
+     let qs = [
+          {
+               message:"Product Name",
+               name:"prodName",
+               type:"input"
+          },
+          {
+               message: "Product department?",
+               type:"list",
+               name:"deptID",
+               choices:function(){
+                    return depts;
+               }
+          },
+          {
+               message:"Price?",
+               name:"price",
+               type:"input",
+               validate: function (value) {
+                    var pass = !isNaN(value) && parseInt(value) > 0;
+                    if (pass) {
+                         return true;
+                    } else {
+                         return "Please enter a valid price";
+                    }
+               }
+          },
+          {
+               message:"Current Inventory?",
+               name:"qty",
+               type:"input",
+               validate: function (value) {
+                    var pass = !isNaN(value) && parseInt(value) > 0;
+                    if (pass) {
+                         return true;
+                    } else {
+                         return "Please enter a valid number";
+                    }
+               }
+          },
+          {
+               message:"Target Inventory? (Once current inventory drops to or or 15% of the traget invenotry, it appears in the low inventory report.",
+               name:"targetQty",
+               type:"input",
+               validate: function (value) {
+                    var pass = !isNaN(value) && parseInt(value) > 0;
+                    if (pass) {
+                         return true;
+                    } else {
+                         return "Please enter a valid inventory level";
+                    }
+               }
+          }
 
+     ];
+     
+     function confirmAdd(arg){
+          inq.prompt([{
+               type:"confirm",
+               message: `Proposed new item:\nName: ${arg.prodName}\nDepartment: ${depts.find( o =>{
+                    return arg.deptID == o.value;
+               }).name}\nPrice: ${arg.price}\nQuantity: ${arg.qty}\nTarget Inventory: ${arg.targetQty}\n`,
+               name:"confirmed"
+          }])
+          .then(function(resp){
+               if(resp.confirmed){
+                    con = sql.createConnection(conOpts);
+                    con.connect(function(err){
+                         if (err)throw err;
+                         console.log("connected");
+                    });
+                    let qury = "INSERT INTO products SET ?";
+                    con.query(qury,arg, function(err, results){
+                         if(err) throw err;
+                         console.log("Added", arg.prodName, "to inventory");
+                         con.end();
+                         welcomeScreen();
+                    })
+               }else{
+                    welcomeScreen();
+               }
+          });
+     }
+     getDeptsRet();
 }
 
 let welcomeScreen = function () {
